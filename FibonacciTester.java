ecc7aa3f470406233056803612bede7c6f44dabf
Karan Naik <kn172@rutgers.edu>
2017-09-27 14:47:11 -0400

    Returns true when value is from fibonacci series, similar to fizzbuzz. improvements needed: calculate fibonacci array dynamically based on necessary max value.

--- FibonacciTester.java
+++ FibonacciTester.java
@@ -1,5 +1,63 @@
+//Returns true when value is from fibonacci series, similar to fizzbuzz
+
 public class FibonacciTester{
 	public static void main(String[] args){
-		System.out.println("fib");
+		Fibtest test = new Fibtest(30);
+		System.out.print(test);
+		//output: 0 inFib inFib inFib 4 inFib 6 7 inFib 9 10 11 12 inFib 14 15 16 17 18 19 20 inFib 22 23 24 25 26 27 28 29 30
 	}
+}
+
+class Fibgen{
+	int[] calc;
+	int size;
+	
+	public Fibgen(){
+		size = 10;
+		arrayFib(size);
+	}
+	
+	private void arrayFib(int stop){
+		int[] temp = new int[stop];
+		temp[0] = 1;
+		temp[1] = 1;
+		for (int i = 0; i < stop-2; i++){
+			temp[i+2] = temp[i] + temp[i+1];
+		}
+		//System.out.println(temp[2]);
+		calc = temp;
+	}
+}
+
+class Fibtest {
+	int testlength;
+	int[] fib;
+	
+	public Fibtest(){
+		System.out.println("No value specified, error");
+	}
+	
+	public Fibtest(int testlen){
+		testlength = testlen + 1;
+		Fibgen test = new Fibgen();
+		fib = test.calc;
+	}
+	public String toString(){
+		String out = "";
+		int currIndexFibArray = 0;
+		for (int i = 0; i < testlength; i++){
+			if (i < fib[currIndexFibArray]) {
+				out += (i + " ");
+			}
+			else {
+				out += ("inFib ");
+				while( i == fib[currIndexFibArray]){
+					currIndexFibArray++;
+				}
+			}
+		}
+		return out;
+	}
+
+
 }
