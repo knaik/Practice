547fa508b855954d68074f1ce314627fc6630f2a
Karan Naik <kn172@rutgers.edu>
2017-09-27 13:25:52 -0400

    OO test

--- Test.java
+++ Test.java
@@ -2,6 +2,14 @@ public class Test{
 	public static void main(String[] args){
 		System.out.println("hello world");
 		System.out.println("hello");
+		FizzBuzzer firstFizz = new FizzBuzzer();
 
 	}
+}
+
+class FizzBuzzer{
+	public FizzBuzzer(){
+		System.out.println("buzzer created");
+	};
+	
 }
