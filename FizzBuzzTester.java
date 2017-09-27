671f7053468045275d5e79d3797c933f80d28dc7
Karan Naik <kn172@rutgers.edu>
2017-09-27 14:23:11 -0400

    Make fizzbuzz work for any two factors and stopPoint

--- FizzBuzzTester.java
+++ FizzBuzzTester.java
@@ -3,6 +3,10 @@ public class FizzBuzzTester{
 		FizzBuzzer firstFizz = new FizzBuzzer(20);
 		System.out.println(firstFizz); 
 		//output: 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz
+		FizzBuzzer secondFizz = new FizzBuzzer(4, 7, 30);
+		System.out.println(secondFizz); 
+		//output: 1 2 3 fizz 5 6 buzz fizz 9 10 11 fizz 13 buzz 15 fizz 17 18 19 fizz buzz 22 23 fizz 25 26 27 fizzbuzz 29
+
 	}
 }
 
@@ -22,17 +26,25 @@ class FizzBuzzer{
 		stopPoint = stop + 1;
 	}
 	
+	public FizzBuzzer(int fizzle, int buzzer, int stop){
+		this();
+		fizz = fizzle;
+		buzz = buzzer;
+		stopPoint = stop + 1;
+
+	}
+	
 	public String toString(){
 		String temp = "";
 		for (int i = 1; i < stopPoint; i++){
-			if (i % 3 == 0) {
+			if (i % fizz == 0) {
 				temp += "fizz";
 			}
-			if (i % 5 == 0) {
+			if (i % buzz == 0) {
 				temp += "buzz";
 			}
 			
-			if (i % 3 != 0 && i % 5 != 0){
+			if (i % fizz != 0 && i % buzz != 0){
 				temp += i;
 			}
 			if( i < stopPoint -1) {
